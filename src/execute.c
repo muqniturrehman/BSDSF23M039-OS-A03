17e0cff73783142762c1d99df0be78a5673d702d
root <root@DESKTOP-BE3R72P.localdomain>
2025-11-11 20:32:18 +0500

    feat: add background process execution with & (Feature 6)

--- src/execute.c
+++ src/execute.c
@@ -8,40 +8,67 @@
 #include "shell.h"
 int execute(char* arglist[]) {
     int status;
-    pid_t cpid = fork();
+    pid_t cpid;
+    int background = 0;
+
+    // Check if last argument is "&"
+    int i = 0;
+    while (arglist[i] != NULL) i++;
+    if (i > 0 && strcmp(arglist[i - 1], "&") == 0) {
+        background = 1;
+        arglist[i - 1] = NULL; // remove "&"
+    }
+
+    cpid = fork();
+
+    if (cpid == -1) {
+        perror("fork failed");
+        return 1;
+    }
 
     if (cpid == 0) { // child
-        // check for redirection
-        for (int i = 0; arglist[i] != NULL; i++) {
-
-            // Output redirection >
-            if (strcmp(arglist[i], ">") == 0) {
-                int fd = open(arglist[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (fd < 0) { perror("open"); exit(1); }
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-                arglist[i] = NULL; // cut off from execvp
-                break;
-            }
-
-            // Input redirection <
-            if (strcmp(arglist[i], "<") == 0) {
-                int fd = open(arglist[i+1], O_RDONLY);
-                if (fd < 0) { perror("open"); exit(1); }
-                dup2(fd, STDIN_FILENO);
-                close(fd);
-                arglist[i] = NULL;
-                break;
-            }
-        }
-
         execvp(arglist[0], arglist);
         perror("exec failed");
         exit(1);
-    } else if (cpid > 0) {
-        waitpid(cpid, &status, 0);
-    } else {
-        perror("fork failed");
+    } 
+    else { // parent
+        if (background) {
+            printf("[BG] Process running in background (PID: %d)\n", cpid);
+        } else {
+            waitpid(cpid, &status, 0);
+        }
+    }
+
+    return 0;
+}
+
+int execute_pipe(char ***cmds) {
+    int fd[2];
+    int in_fd = 0; // first command reads from stdin
+    pid_t pid;
+    int i = 0;
+
+    while (cmds[i] != NULL) {
+        pipe(fd);
+        pid = fork();
+
+        if (pid == 0) {                     // child
+            dup2(in_fd, STDIN_FILENO);      // previous input
+            if (cmds[i+1] != NULL)
+                dup2(fd[1], STDOUT_FILENO); // next output
+            close(fd[0]);
+            execvp(cmds[i][0], cmds[i]);
+            perror("exec failed");
+            exit(1);
+        } else if (pid > 0) {               // parent
+            wait(NULL);
+            close(fd[1]);
+            in_fd = fd[0];                  // next input = this pipe's read end
+            i++;
+        } else {
+            perror("fork");
+            return 1;
+        }
     }
     return 0;
 }
